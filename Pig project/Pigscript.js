ed6b106a6ae0b33fa33adf99c37053cfee4c5404
kikomal <88785583+kikomal@users.noreply.github.com>
2021-09-23 18:18:39 +0530

    Updated new game, dice and hold button

--- Pig project/Pigscript.js
+++ Pig project/Pigscript.js
@@ -1,30 +1,92 @@
-"use strict";
-const score0E1 = document.querySelector("#score--0");
-const score1E1 = document.getElementById("#score--1");
-const diceE1 = document.querySelector(".dice");
-const btnNew = document.querySelector(".btn--new");
-const btnRoll = document.querySelector(".btn--roll");
-const btnHold = document.querySelector(".btn--hold");
+'use strict';
+const player0E1 = document.querySelector('.player--0');
+const player1E1 = document.querySelector('.player--1');
+const score0E1 = document.querySelector('#score--0');
+const score1E1 = document.getElementById('score--1');
+const current0E1 = document.getElementById('current--0');
+const current1E1 = document.getElementById('current--1');
+const diceE1 = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 //starting conditions
-score0E1.textcontent(0);
-score1E1.textcontent(0);
-diceE1.classList.add("hidden");
-let currentScore = 0; //we are decalring this variable here and not in function because whenever we call function the value will be added as 0 which we do not want
+let scores, currentScore, activePlayer, playing;
 
-btnRoll.addEventListener("click", function () {
-  //Rolling a dice
-  const dice = Math.trunc(Math.random() * 6) + 1;
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
 
-  //displaying the dice value
-  diceE1.classList.remove("hidden");
-  diceE1.src = `dice--${dice}.png`;
+  score0E1.textContent = 0;
+  score1E1.textContent = 0;
+  current0E1.textContent = 0;
+  current1E1.textContent = 0;
 
-  //chcek if dice value is 1: if yes switch the player
-  if (dice !== 1) {
-    //add the value of score in total
-    currentScore += dice;
-  } else {
-    //switch the players
+  diceE1.classList.add('hidden');
+  player0E1.classList.add('player--active');
+  player1E1.classList.remove('player--active');
+  player0E1.classList.remove('player--winner');
+  player1E1.classList.remove('player--winner');
+};
+init();
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  //toggle will chcek if player--active class is with player0E1 if not then add that class and if already there then will remove that class
+  player0E1.classList.toggle('player--active');
+  player1E1.classList.toggle('player--active');
+};
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  if (playing) {
+    //Generating a random number from dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+
+    //Display the dice
+    diceE1.classList.remove('hidden');
+    diceE1.src = `dice-${dice}.png`;
+    //console.log(dice);
+
+    //check if dice value is 1:
+    if (dice !== 1) {
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    }
+    //if yes, switch the player
+    else {
+      switchPlayer();
+    }
   }
 });
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    //add currentscore to Score
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    //chk if socre>=100: declare winner and finish the game
+    if (scores[activePlayer] >= 100) {
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+      diceE1.classList.add('hidden');
+
+      playing = false;
+    } else {
+      //else: switch the player
+      switchPlayer();
+    }
+  }
+});
+
+btnNew.addEventListener('click', init);
